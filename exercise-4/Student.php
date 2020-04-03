6803189e25293886814f4c2a00867fdc131462ef
Akos Szabo-Gali <szabogaliakos@gmail.com>
2020-04-03 11:20:48 +0200

    [EXERCISE-4] Finished

--- exercise-4/Student.php
+++ exercise-4/Student.php
@@ -1,7 +1,65 @@
 <?php
 
-
 class Student
 {
+    public static $students = array();
 
+    private $name;
+    private $class;
+    private $scores;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getScores()
+    {
+        return $this->scores;
+    }
+
+    public function __construct($name, $class, $scores)
+    {
+        $this->name = $name;
+        $this->class = $class;
+        $this->scores = $scores;
+    }
+
+    public static function LoadStudentsFromXml($path){
+        $xml=simplexml_load_file("resources/students.xml") or die("Error: Cannot create object");
+        foreach ($xml->children() as $studentNode){
+            $name = $studentNode->name;
+            $class = $studentNode->class;
+            $scoreNodes = $studentNode->scores->children();
+            $scores = array(
+                strval($scoreNodes[0]['name']) => intval($scoreNodes[0]),
+                strval($scoreNodes[1]['name']) => intval($scoreNodes[1]),
+                strval($scoreNodes[2]['name']) => intval($scoreNodes[2])
+            );
+            array_push(Student::$students, new Student($name, $class, $scores));
+        }
+    }
+
+    public static function getHighestScore($name){
+        $highestScoreStudent = Student::$students[0];
+        foreach (Student::$students as $student){
+            if ((int)$highestScoreStudent->getScores()[$name] < (int)$student->getScores()[$name]){
+
+                $highestScoreStudent = $student;
+            }
+        }
+        return $highestScoreStudent;
+    }
+
+    public function __toString()
+    {
+        $studentRow = "<tr>";
+        $studentRow = "<td>".$this->name."</td>";
+        $studentRow .= "<td>".$this->class."</td>";
+        $studentRow .= "<td>".$this->scores["CSS"]."</td>";
+        $studentRow .= "<td>".$this->scores["PHP"]."</td>";
+        $studentRow .= "<td>".$this->scores["Java"]."</td>";
+        $studentRow .= "</tr>";
+        return $studentRow;
+    }
 }
--- exercise-4/index.php
+++ exercise-4/index.php
@@ -1,10 +1,38 @@
+<?php
+    require_once "Student.php";
+    Student::LoadStudentsFromXml("resources/students.xml");
+    usort(Student::$students, function ($a, $b) { return $a->getScores()["PHP"] - $b->getScores()['PHP']; });
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Students</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Class</th>
+            <th>CSS score</th>
+            <th>PHP score</th>
+            <th>Java score</th>
+        </tr>
+        <?php
+            foreach (Student::$students as $student){
+                echo $student;
+            }
+        ?>
+    </table>
+    <label>Highest score in CSS:</label>
+    <label>
+        <b>
+            <?php
+                echo Student::getHighestScore("PHP")->getName();
+            ?>
+        </b>
+    </label>
 </body>
 </html>
