3ae959c4fc013797a517a1fce373930f53ed25d3
Akos Szabo-Gali <szabogaliakos@gmail.com>
2020-04-03 05:11:32 +0200

    [EXERCISE-3] Logic was implemented

--- exercise-3/index.php
+++ exercise-3/index.php
@@ -1,3 +1,18 @@
+<?php
+    $output = "";
+    $input = "Today is a nice day!";
+    $find = "Today";
+    $replace = "Tomorrow";
+
+    if (isset($_GET["input"]) && isset($_GET["find"]) && isset($_GET["replace"])){
+        $input = $_GET["input"];
+        $find = $_GET["find"];
+        $replace = $_GET["replace"];
+
+        $output = str_replace($find, $replace, $input);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,18 +25,18 @@
         <h3>REPLACE STRING</h3>
         <div class="light-background">
             <label for="input">Input</label><br>
-            <textarea id="input" type="text">Today is a nice day!</textarea><br>
+            <textarea name="input" id="input" type="text"><?php echo $input ?></textarea><br>
             <div class="flex-container">
                 <div class="flex-element">
                     <label for="find">Find:</label>
-                    <input id="find" type="text" value="Today">
+                    <input name="find" id="find" type="text" value="<?php echo $find ?>">
                 </div>
                 <div class="flex-element">
                     <label for="replace">Replace:</label>
-                    <input id="replace" type="text" value="Today"><br>
+                    <input name="replace" id="replace" type="text" value="<?php echo $replace ?>"><br>
                 </div>
             </div>
-            <textarea id="output" type="text"></textarea>
+            <textarea id="output" type="text"><?php echo $output ?></textarea>
             <hr>
             <button type="submit">Submit</button>
         </div>
